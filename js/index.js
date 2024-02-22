c73d55181c2a356f6d5195bf0663fe2ccf81da63
golfyplayz <jackson.verdoorn@gmail.com>
2024-02-22 13:09:18 -0600

    redo the main page

--- js/index.js
+++ js/index.js
@@ -1,16 +1,24 @@
 const submit = document.getElementById('submit');
 const clear = document.getElementById('clear');
-let output = document.getElementById('output');
+const stockInt = document.getElementById('stockInt');
+const stockFin = document.getElementById('stockFin');
+const stockAmt = document.getElementById('stockAmt');
+const outputDiv = document.getElementById('output');
 
 const calculate = (inital, final, amount) => {
     let result = (final - inital) * amount;
     return result;
 }
-const reset = () => {
-    document.getElementById('stockInt').value = '';
-    document.getElementById('stockFin').value = '';
-    document.getElementById('stockAmt').value = '';
-    output.style.visibility = 'hidden';
+
+const checkIfOpen = () => {
+    if(document.getElementById('outputHeader')) {
+        console.log('true');
+        return true;
+    } else {
+        console.log('false');
+        return false;
+    }
+
 }
 
 submit.addEventListener('click', (event) => {
@@ -20,8 +28,26 @@ submit.addEventListener('click', (event) => {
     let amnt = document.getElementById('stockAmt').value;
     let total = calculate(init, fin, amnt).toString();
     console.log(total);
-    output.style.visibility = 'visible';
-    document.getElementById('outputText').innerHTML = total;
+    let clear = checkIfOpen();
+    if(!clear) {
+        event.preventDefault();
+        console.log('clear');
+        let header = document.createElement('h1');
+        outputDiv.appendChild(header);
+        header.id = 'outputHeader'
+        header.innerHTML = 'Money made';
+        let outputAmnt = document.createElement('h3')
+        outputDiv.appendChild(outputAmnt);
+        outputAmnt.id = 'outputAmnt';
+        outputAmnt.innerHTML = total;
+    }
+    else {
+        console.log('div');
+        let header = document.getElementById('outputHeader');
+        let outputAmnt = document.getElementById('outputAmnt');
+        outputAmnt.remove();
+        header.remove();
+    }
 })
 
 clear.addEventListener('click', (event) => {
