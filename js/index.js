57956f84fc29f6819e0e03c7dd12169f61600587
golfyplayz <jackson.verdoorn@gmail.com>
2024-03-06 10:06:18 -0600

    redid javascript for calculator output

--- js/index.js
+++ js/index.js
@@ -4,14 +4,16 @@ const stockFin = document.getElementById('stockFin');
 const stockAmt = document.getElementById('stockAmt');
 const outputDiv = document.getElementById('output');
 const adminButton = document.getElementById('admin');
+let header;
+let outputAmnt;
 
 const calculate = (inital, final, amount) => {
     let result = (final - inital) * amount;
     return result;
 }
 
-const checkIfOpen = () => {
-    if(document.getElementById('outputHeader')) {
+const checkIfShouldRemove = () => {
+    if(stockInt.value==='' || stockFin.value==='' || stockAmt.value==='') {
         console.log('true');
         return true;
     } else {
@@ -28,21 +30,22 @@ submit.addEventListener('click', (event) => {
     let amnt = document.getElementById('stockAmt').value;
     let total = calculate(init, fin, amnt).toFixed(2).toString();
     console.log(total);
-    let clear = checkIfOpen();
-    if(!clear) {
+    if(!checkIfShouldRemove()) {
         event.preventDefault();
+        if(!document.getElementById('outputHeader') && !document.getElementById('outputAmnt')) {
         console.log('clear');
-        let header = document.createElement('h1');
+        header = document.createElement('h1');
         outputDiv.appendChild(header);
         header.id = 'outputHeader'
+        outputAmnt = document.createElement('h3')
+        outputDiv.appendChild(outputAmnt);
+        outputAmnt.id = 'outputAmnt';
+        }
         if(total >= 0) {
             header.innerHTML = 'Money made';
         } else {
             header.innerHTML = 'Money lost';
         }
-        let outputAmnt = document.createElement('h3')
-        outputDiv.appendChild(outputAmnt);
-        outputAmnt.id = 'outputAmnt';
         if (total[0] === '-') {
             total = total.substr(1);
             outputAmnt.innerHTML = '-$'+total;
